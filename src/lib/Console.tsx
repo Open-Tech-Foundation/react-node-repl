3a946f08142646d0883d64a24a16e4786723f7a3
Thanga Ganapathy <ganapathy888@gmail.com>
2024-02-22 20:52:32 +0530

    Highlight error messages in console

--- src/lib/Console.tsx
+++ src/lib/Console.tsx
@@ -4,21 +4,41 @@ type Props = {
   logs: string[];
 };
 
+function isError(str: string) {
+  const errPattern = [
+    "Error:",
+    "SyntaxError:",
+    "TypeError:",
+    "ReferenceError:",
+    "RangeError:",
+  ];
+  return errPattern.some((s) => str.startsWith(s));
+}
+
 export default function Console({ logs }: Props) {
   const renderLogs = () => {
-    return logs.map((l, i) => (
-      <div
-        key={i}
-        style={{
-          color: "white",
-          padding: "3px",
-          paddingLeft: "10px",
-          borderBottom: "1px solid rgb(44, 44, 44)",
-        }}
-      >
-        {stripAnsi(l)}
-      </div>
-    ));
+    return logs.map((l, i) => {
+      const err = isError(l);
+      const txt = stripAnsi(l);
+      return (
+        <div
+          key={i}
+          style={{
+            color: err ? "rgb(255, 128, 128)" : "white",
+            padding: "3px",
+            paddingLeft: "10px",
+            borderBottom: "1px solid rgb(44, 44, 44)",
+            backgroundColor: err ? "rgb(41, 0, 0)" : "initial",
+          }}
+        >
+          {err ? (
+            <pre style={{ margin: 0, whiteSpace: "pre-wrap" }}>{txt}</pre>
+          ) : (
+            txt
+          )}
+        </div>
+      );
+    });
   };
 
   return (
